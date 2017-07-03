3dce3b19c5bdb1299cd2b0053d07a0216d1104f2
LiveAlone <yaoqijunmail@gmail.com>
2017-07-04 00:12:01 +0800

    add all

---
+++ src/org/yqj/demo/Main.java
@@ -0,0 +1,9 @@
+package org.yqj.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("current condition java9 version");
+    }
+}
